22d68e0ef7bfabf76295225eab52d5edc564a7df
OlabamipeTaiwo <teeola48@gmail.com>
2022-03-12 09:32:12 +0100

    dev::fixed search input mobile style

--- src/components/SearchEvents.vue
+++ src/components/SearchEvents.vue
@@ -12,7 +12,7 @@
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapActions } from "vuex"
 
 export default {
   name: "SearchEvents",
@@ -31,3 +31,17 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.search__input {
+  @apply flex items-center p-4 bg-white border-grey-two border-2 rounded-lg w-full sm:max-w-md;
+
+  svg {
+    @apply flex-shrink-0;
+  }
+
+  input {
+    @apply ml-2 flex-grow bg-transparent text-grey-four outline-none text-sm font-display placeholder-grey-four;
+  }
+}
+</style>
